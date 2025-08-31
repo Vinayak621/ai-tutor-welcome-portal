3aadc8674acef5528439106a0820440ff839e129
vinayaka <vinayaka.bv@nighthack.in>
2025-08-31 21:37:22 +0530

    url env replace

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -89,7 +89,7 @@ export const LoginForm = () => {
     // });
 
   const handleGoogleSignIn=()=>{
-    window.location.href = "http://localhost:3000/api/auth/google";
+    window.location.href = `${import.meta.env.VITE_API_URL}}/api/auth/google`;
   }
 
   return (
